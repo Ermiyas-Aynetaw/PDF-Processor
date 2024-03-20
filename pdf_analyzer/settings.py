c9e84052845be0e0088f4be606d7c9ba1da80df0
Ermiyas-Aynetaw <87368573+Ermiyas-Aynetaw@users.noreply.github.com>
2024-03-20 16:24:55 +0300

    2nd commit

--- pdf_analyzer/settings.py
+++ pdf_analyzer/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = "django-insecure-*vu4=%)(#a5+7hkm@@=cri5vu&ysvyo^%on)y_h--d0pgv1q5z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -132,6 +134,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+MEDIA_URL = ''
+
+STATICFILES_DIRS = [BASE_DIR / 'static'
+                   
+                 ]
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'static/images')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
